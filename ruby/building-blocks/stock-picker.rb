a33b08490a498be8575ff0ebc2f1d450bfc06147
jeckt <yeemho@gmail.com>
2018-03-07 11:40:20 -0500

    optimise logic and code for stockpicker

--- ruby/building-blocks/stock-picker.rb
+++ ruby/building-blocks/stock-picker.rb
@@ -1,53 +1,32 @@
-#loop through each price on buy day i
-      #loop through all following days (potential sell days) and find the difference -> save it into profit_by_sellday object. 
-      #End loop
-      #take the sell_day:profit k-v pair from profit_by_sellday object and save it into profit_by_buyday[buy_day] = {sell_day:profit}
-    #end loop
-    #take the buy and sell days with max profit
-    #If max profit is positive then result will equal these days of trade
-    #otherwise result is nil (no profitable trades)
-    
-def stock_picker(prices)
-  if prices.length < 2
-    result = nil
-  elsif prices.max == prices.min 
-    result = nil
-  else
-    result = optimal_profit(prices)
-  end 
-  result
-end
-
-def optimal_profit(prices)
-  max_day = prices.index(prices.max)
-  min_day = prices.index(prices.min)
-    
-  if max_day > min_day
-    result = [min_day,max_day]
-  else
-    profits = {}
-    for i in 0..(prices.length-2) do
-      profits_by_sellday = {}
-      for j in (i+1)..(prices.length-1) do
-        profits_by_sellday[j] = prices[j] - prices[i]
-      end
-      max_profit_by_buyday = profits_by_sellday.select {|key,val| val == profits_by_sellday.values.max && val > 0}
-      profits[i] = max_profit_by_buyday unless max_profit_by_buyday.empty?
-    end
-    max_profit = 0
-    profits.each do |buyday,sell_info|
-      sellday = sell_info.keys[0]
-      profit = sell_info[sellday]
+def stockpicker(array)
+  #can only sell after buying (only numbers to the right matter)
+  #prices compare to prices within the array (nest another loop?)
+  
+  #take array of prices and start from first position. For each position
+    #take each price to its right and find the difference of a price to the right
+    #if difference is the largest so far, then assign max diff and save the buy index and sell index into result array
+    #repeat for all prices except the last
+  #return result
+  
+  optimal_days=[] #initialize result array
+  
+  max_profit = 0 #initialize maximum profit so far
+  
+  array.each_with_index {|price,index|  #iterate over array, assuming each iteration is the buy day
+    buyprice = price
+    buyday = index
+    nextdays = array.drop(buyday) #the possible prices and days we can sell assuming a given buy price and day
+    nextdays.each_with_index {|sellprice, sellday| 
+      profit = sellprice - buyprice 
       if profit > max_profit
-        result = [buyday,sellday]
         max_profit = profit
-        p max_profit
+        optimal_days= [buyday , sellday + buyday]
       end
-    end
-  end
-  result
+    }
+  }
+  
+  optimal_days
+  
 end
 
-stock_picker([17,16,15,16,3,6,9,15,16,3,8,6,10,2,1])
-
-
+p stockpicker([9,19,8,3,4,5,6,7,8,1,19,0])
