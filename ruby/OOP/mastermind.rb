66c09e3d2c955596089141f43ef0e715bd59710a
jeckt <yeemho@gmail.com>
2018-03-28 14:48:56 -0400

    Check valid input

--- ruby/OOP/mastermind.rb
+++ ruby/OOP/mastermind.rb
@@ -5,33 +5,68 @@
 
 module Mastermind
   
+    def valid?
+      if self.all? {|x| x.is_a? Integer}
+        return true if self.min >= 1 && self.max <= 6
+      end
+      return false
+    end
+    
   class Game
-    @@moves = []
+    
     def initialize
-      @random_code = [1,2,4,5]#Array.new(4) { rand(1..6) }
-      puts "Welcome to Mastermind. Try to guess the secret pattern which consists of 4 numbers, each between 1 and 6." 
+      @random_code = Array.new(4) { rand(1..6) }
+      @moves = []
+      @result = {}
+      puts "Welcome to Mastermind..."
+      puts "Try to guess the secret code which consists of 4 numbers, each from 1 to 6." 
       puts "Enter your guess with each number separated by a comma. E.g. 1,2,3,4. You have 12 guesses."
     end
     
     def get_guess
       puts "What is your guess?"
-      input = gets.chomp.split(',').map {|x| x.to_i } 
-      Guess.new(input)
+      input = gets.chomp.split(',').map {|x|   
+        x.to_i 
+      }
+      play_round(input)        
+    end
+    
+    def play_round(input)
+    
+      if input.valid?
+        @guess = Guess.new(input)
+        @result = @guess.check_guess(@random_code)
+        feed_back(@guess, @result)
+      else
+        puts "Invalid guess, ensure you enter 4 numbers, each from 1 to 6."
+        get_guess
+      end
+      
     end
     
     def play
-      #until won? or round = 12 keep looping through
-      guess = get_guess   
-      result = guess.check_guess(@random_code)
-      feed_back(guess, result)
+      until game_over do
+        guess = get_guess
+      end
+      puts game_over + " Answer: #{@random_code}"
     end 
     
     def feed_back (guess, result)
-      @@moves.push("Round #{Guess.round}: #{guess.pattern} | Pegs: #{result[:red]} Red, #{result[:white]} White")
+      @moves.push("Round #{Guess.round}: #{guess.pattern} | Pegs: #{result[:red]} Red, #{result[:white]} White")
       puts "+++++++++++"
       puts "CODEBOARD"
       puts "+++++++++++"
-      @@moves.each{|result| puts result}
+      @moves.each{|result| puts result}
+    end
+    
+    def game_over
+     return "You cracked the code!" if won?
+     return "Game over." if Guess.round >= 12
+     return false
+    end
+    
+    def won?
+      return @result[:red] == 4
     end
      
   end
@@ -49,13 +84,13 @@ module Mastermind
       @@round
     end
     
-    def check_guess(code)
-      @red_pegs = 0
+    def check_guess(code)     
+      red_pegs = 0
       @pattern.each_with_index { |x,i| 
-        @red_pegs +=1 if x == code[i]
+        red_pegs +=1 if x == code[i]
       }
-      @white_pegs = (@pattern & code).length - @red_pegs
-      return {red: @red_pegs, white: @white_pegs}
+      white_pegs = 4 - (@pattern - code).length - red_pegs
+      return {red: red_pegs, white: white_pegs}
     end
   end
 
