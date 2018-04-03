de23624b3192745a1003816073979a6673d37666
jeckt <yeemho@gmail.com>
2018-04-03 14:54:19 -0400

    Refactor code to allow role choice

--- ruby/OOP/mastermind.rb
+++ ruby/OOP/mastermind.rb
@@ -5,51 +5,94 @@
 
 module Mastermind
   
+    def choose_player
+      puts "Would you like to create or guess the code?"
+      choice = gets.chomp
+      return assign_roles(choice)
+    end
+    
+    def assign_roles(human_choice)
+      case human_choice
+        when "guess"
+          creator = Player.new("computer")
+          guessor = Player.new("human")
+        when "create"
+          creator = Player.new("human")
+          guessor = Player.new("computer")
+        else
+          puts "Choose either guess or create"
+          choose_player
+      end
+      return { creator: creator, guessor: guessor }
+    end
+    
     def valid?
-      if self.all? {|x| x.is_a? Integer}
+      if self.length == 4 && self.all? {|x| x.is_a? Integer}
         return true if self.min >= 1 && self.max <= 6
       end
       return false
     end
     
+   
+  class Player
+    attr_reader :type
+    def initialize(type)
+      @type= type
+    end
+    
+    def get_code
+      case self.type
+        when "human" then return get_input
+        when "computer" then return Array.new(4){rand(1..6)}
+      end
+    end
+    
+    def get_input
+      input = gets.chomp.split(',').map {|x| x.to_i}
+      if input.valid?
+        return input
+      else
+        puts "Invalid entry. Ensure you enter 4 numbers separated by commas, each from 1 to 6. Try again:"
+        get_code
+      end
+    end
+    
+  end
+
+    
   class Game
     
-    def initialize
-      @random_code = [6,4,4,5] #Array.new(4) { rand(1..6) }
+    def initialize(players)
+      @creator = players[:creator]
+      @guessor = players[:guessor]
       @moves = []
-      @result = {}
-      puts "Welcome to Mastermind..."
-      puts "Try to guess the secret code which consists of 4 numbers, each from 1 to 6." 
-      puts "Enter your guess with each number separated by a comma. E.g. 1,2,3,4. You have 12 guesses."
-    end
-    
-    def get_guess
-      puts "What is your guess?"
-      input = gets.chomp.split(',').map {|x|   
-        x.to_i 
-      }
-      play_round(input)        
-    end
-    
-    def play_round(input)
-    
-      if input.valid?
-        @guess = Guess.new(input)
-        @result = @guess.check_pattern(@random_code)
-        feed_back(@guess, @result)
-      else
-        puts "Invalid guess, ensure you enter 4 numbers, each from 1 to 6."
-        get_guess
-      end
-      
+      @result = {}          
     end
     
     def play
+      generate_secret_code
       until game_over do
         guess = get_guess
       end
       puts game_over + " Answer: #{@random_code}"
     end 
+    
+    def generate_secret_code
+      puts "Enter your 4-digit secret code" if @creator.type == "human"
+      @random_code = @creator.get_code
+    end
+    
+    def get_guess
+      puts "What is your guess?"
+      input = @guessor.get_code
+      play_guess(input)        
+    end
+    
+    def play_guess(input)    
+        @guess = Guess.new(input)
+        @result = @guess.check_pattern(@random_code)
+        feed_back(@guess, @result)      
+    end
     
     def feed_back (guess, result)
       @moves.push("Round #{Guess.round}: #{guess.pattern} | Pegs: #{result[:red]} Red, #{result[:white]} White")
@@ -67,12 +110,10 @@ module Mastermind
     
     def won?
       return @result[:red] == 4
-    end
-     
-  end
+    end 
+  end  
   
-  class Guess 
-  
+  class Guess
     attr_reader :pattern, :round
     @@round = 0
     def initialize(array)
@@ -105,12 +146,19 @@ module Mastermind
       end
       white_pegs = 4 - red_pegs - a.length
       return { red: red_pegs, white: white_pegs}
-    end
-    
+    end  
   end
+  
 end
+
+
 
 include Mastermind
 
-game = Game.new
+puts "Welcome to Mastermind..."
+puts "One player will decide the secret code (consisting of 4 numbers, each from 1 to 6). The other will have 12 rounds to break the code." 
+
+players = choose_player
+game = Game.new(players)
 game.play
+
